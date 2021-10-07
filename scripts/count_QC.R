6dda2f6677799e639e7c3433b5c9c828ac5bd0f1
Hannah Dewey <57405854+deweyhannah@users.noreply.github.com>
2021-10-07 15:41:45 -0400

    Update count_QC.R

--- scripts/count_QC.R
+++ scripts/count_QC.R
@@ -42,11 +42,12 @@ for(celltype in unique(cell_reps$celltype)){
 	message("aggregating counts")
 	agg_count <- aggregate(. ~Oligo, data=dataCount[,-1], FUN=sum)
 	agg_count$means <- rowMeans(agg_count[,reps])
+	mean_bound <- as.numeric(quantile(agg_count$means, seq(0,1,0.01))[91])
 	agg_ct_gt20 <- nrow(agg_count[which(agg_count$means > 20),])
 	total_oligos <- nrow(agg_count)
 	
-	agg_rep_bcs[[celltype]] <- ggplot(agg_rep_bc, aes(x=Freq)) + geom_histogram(bins=200) + geom_vline(xintercept=10, col="red") + xlab("Barcodes per aggregated Oligo") + ggtitle(paste0("Aggregated Barcode Count\n",agg_gt10," Oligos with > 10 Barcodes")) + theme_light()
-	agg_rep_counts[[celltype]] <- ggplot(agg_count, aes(x=means)) + geom_histogram() + geom_vline(xintercept=20, col="red") + xlab("Mean Count per  aggregated Oligo") + ggtitle(paste0("Mean Oligo Counts\n", agg_ct_gt20," Oligos with Mean Count > 20")) + theme_light()
+	agg_rep_bcs[[celltype]] <- ggplot(agg_rep_bc, aes(x=Freq)) + geom_histogram(bins=200) + geom_vline(xintercept=10, col="red") + xlim(0,mean_bound) + xlab("Barcodes per aggregated Oligo") + ggtitle(paste0("Aggregated Barcode Count\n",agg_gt10," Oligos with > 10 Barcodes")) + theme_light()
+	agg_rep_counts[[celltype]] <- ggplot(agg_count, aes(x=means)) + geom_histogram(bins=300) + geom_vline(xintercept=20, col="red") + xlab("Mean Count per  aggregated Oligo") + ggtitle(paste0("Mean Oligo Counts\n", agg_ct_gt20," Oligos with Mean Count > 20")) + theme_light()
 	
 	for(rep in reps){
 		### Individual replicate barcode histograms
