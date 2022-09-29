9e30b9ad78f6edbde234a834219b68b14ee20c9f
Hannah Dewey <57405854+deweyhannah@users.noreply.github.com>
2022-09-29 11:53:16 -0400

    Update fill_MPRAmatch_json.sh

--- setup/fill_MPRAmatch_json.sh
+++ setup/fill_MPRAmatch_json.sh
@@ -11,6 +11,6 @@ FASTA="${FLOC}/setup/${PROJ}_reference.fasta.gz"
 
 cp <path/to/cloned/repository>/inputs/MPRAmatch_sample_inputs.json ../submission/MPRAmatch_${PROJ}_inputs.json #If using different organization change from submission to desired location
 
-jq --arg PROJ ${PROJ} --arg FLOC ${FLOC} --arg OUT ${OUT} --arg READ1 ${READ1} --arg READ2 ${READ2} --arg FASTA ${FASTA} -M '. + {"MPRAmatch.read_a":'\"${READ1}/\"',
-    "MPRAmatch.read_b":'\"${READ2}/\"', "MPRAmatch.reference_fasta":'\"${FASTA}/\"',
-    "MPRAmatch.id_out":'\"${OUT}/\"',"MPRAmatch.out_directory":'\"${FLOC}/\"'}' ../submission/MPRAmatch_${PROJ}_inputs.json > <path/to/cloned/repository>/inputs/MPRAmatch/MPRAmatch_${PROJ}_inputs.json #change submission to match above
+jq --arg PROJ ${PROJ} --arg FLOC ${FLOC} --arg OUT ${OUT} --arg READ1 ${READ1} --arg READ2 ${READ2} --arg FASTA ${FASTA} -M '. + {"MPRAmatch.read_a":'\"${READ1}\"',
+    "MPRAmatch.read_b":'\"${READ2}\"', "MPRAmatch.reference_fasta":'\"${FASTA}\"',
+    "MPRAmatch.id_out":'\"${OUT}\"',"MPRAmatch.out_directory":'\"${FLOC}/\"'}' ../submission/MPRAmatch_${PROJ}_inputs.json > <path/to/cloned/repository>/inputs/MPRAmatch/MPRAmatch_${PROJ}_inputs.json #change submission to match above
