691d17e62e4b77b8a8788198d8147bb08a5b76b3
Hannah Dewey <57405854+deweyhannah@users.noreply.github.com>
2023-05-18 11:52:22 -0400

    Update fill_MPRAcount_json.sh

--- setup/fill_MPRAcount_json.sh
+++ setup/fill_MPRAcount_json.sh
@@ -9,9 +9,10 @@ ACC="${FLOC}/setup/<path/to/replicate_index>"
 ID=`awk '{print $2}' ${ACC} | uniq | awk '{aggr=aggr",\""$1"\""} END {print aggr}' | sed 's/,//'`
 REPS=`awk '{print $2}' ${ACC} | uniq | awk '{aggr=aggr",\"'${FLOC}'/fastq/"$1".fastq.gz\""} END {print aggr}' | sed 's/,//'`
 PARS="${FLOC}/<MPRAmatch_output_dir>/${OUT}.merged.match.enh.mapped.barcode.ct.parsed" #adjust directory to match directory organization
+WORK="<path/to/MPRA_oligo_barcode_pipelin/scripts"
 
 cp <path/to/cloned/repository>/inputs/MPRAcount_sample_inputs.json ../submission/MPRAcount_${PROJ}_inputs.json #If using different organization change from submission to desired location
 
-jq --arg PROJ ${PROJ} --arg FLOC ${FLOC} --arg ACC ${ACC} --arg OUT ${OUT} --arg ID ${ID} --arg REPS ${REPS} --arg PARS ${PARS} -M '. + {"MPRAcount.out_directory":'\"${FLOC}/\"',
-    "MPRAcount.id_out":'\"$OUT\"', "MPRAcount.parsed":'\"$PARS\"', "MPRAcount.acc_id":'\"$ACC\"', 
+jq --arg PROJ ${PROJ} --arg FLOC ${FLOC} --arg ACC ${ACC} --arg OUT ${OUT} --arg ID ${ID} --arg REPS ${REPS} --arg PARS ${PARS} --arg WORK ${WORK} -M '. + {"MPRAcount.out_directory":'\"${FLOC}/\"',
+    "MPRAcount.id_out":'\"$OUT\"', "MPRAcount.parsed":'\"$PARS\"', "MPRAcount.acc_id":'\"$ACC\"', "MPRAcount.working_directory":'\"$WORK\"', 
     "MPRAcount.replicate_fastq":'\[$REPS\]', "MPRAcount.replicate_id":'\[$ID\]'}' ../submission/MPRAcount_${PROJ}_inputs.json > <path/to/cloned/repository>/inputs/MPRAcount/MPRAcount_${PROJ}_inputs.json #change submission to match above
