43b7c3aa84fc385ae45f7761166ea4b498189f19
Hannah Dewey <hannah.dewey@jax.org>
2023-06-14 12:42:51 -0400

    Update make_counts.py

--- scripts/make_counts.py
+++ scripts/make_counts.py
@@ -50,7 +50,7 @@ with open("%s/%s.match" % (current_path, out_id), "w") as match_oligo:
 
                     # Check for barcode presence in the dictionary
                     # if bc_seq in BC_dict:
-                    match_oligo.write("%s\t%s\t%s\n" % (record.name,bc_seq,BC_dict[bc_seq]))
+                    match_oligo.write("%s\t%s\n" % (record.name,bc_seq))
                     #if bc_seq not in BC_dict:
                     #    SeqIO.write(record, reject_fastq, "fastq")
                     #    reject_bc.write("%s\t%s\n" % (record.name,bc_seq))
